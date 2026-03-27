8b6cebc75a909a7868a2d0c019734d03d2267f4e
a3524110303-cpu <a3524110303@alumno.uttehuacan.edu.mx>
2026-03-26 19:46:38 -0600

    SE AGREGO LA MIGRACION QUE ALTERA LA TABLA DE NOTIFICAICONES

---
+++ database/migrations/2026_03_26_194408_alter_notificaciones_enum_values.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        public function up()
+{
+    DB::statement("ALTER TABLE notificaciones MODIFY COLUMN tipo ENUM('recordatorio', 'confirmacion', 'cancelacion', 'push', 'reagenda') NULL");
+    DB::statement("ALTER TABLE notificaciones MODIFY COLUMN estado ENUM('pendiente', 'enviado', 'leido', 'no_leida') NULL");
+}
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
