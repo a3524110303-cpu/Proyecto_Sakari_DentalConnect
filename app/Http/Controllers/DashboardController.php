9f74379a40ef89f79f8416ba5008637bfdc83aed
a3524110303-cpu <a3524110303@alumno.uttehuacan.edu.mx>
2026-04-07 01:07:14 -0600

    Correccion de filtracion de Publicidades de la api de la aplicacion movil y correccion de controlador del resitro de movimientos en la tarjeta de citas

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -186,16 +186,23 @@ class DashboardController extends Controller
 
         $idsCitasCiclo = $citasCiclo->pluck('id_cita');
 
-        // QA: no cargar costo de citas futuras en el saldo actual.
-        // Solo se considera "devengado" lo ya atendido o con fecha/hora vencida.
+        // QA: no cargar costo de citas futuras sin movimiento en el saldo actual.
+        // Excepción: si una cita futura YA tiene abonos, se considera activa en cuenta.
         $ahora = now();
         $idsCitasDevengadas = $citasCiclo
-            ->filter(function ($item) use ($ahora) {
+            ->filter(function ($item) use ($ahora, $pagosPorCita) {
+                $idCita = (int) $item->id_cita;
+                $tieneAbonos = ((float) ($pagosPorCita[$idCita] ?? 0)) > 0;
+
                 if (!empty($item->fecha_hora_inicio) && Carbon::parse($item->fecha_hora_inicio)->lte($ahora)) {
                     return true;
                 }
 
-                return in_array(strtolower((string) ($item->estado_cita ?? '')), ['completada'], true);
+                if (in_array(strtolower((string) ($item->estado_cita ?? '')), ['completada'], true)) {
+                    return true;
+                }
+
+                return $tieneAbonos;
             })
             ->pluck('id_cita');
 
